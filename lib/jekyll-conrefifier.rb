3826350c08f28901f09da56c58fe33227929b116
Garen Torikian <gjtorikian@gmail.com>
2015-01-31 15:50:37 -0800

    Add support for filters within data files

--- lib/jekyll-conrefifier.rb
+++ lib/jekyll-conrefifier.rb
@@ -22,7 +22,7 @@ module Jekyll
       old_read(opts)
       @data.each_pair do |key, value|
         if value =~ /\{\{.+?\}\}/
-          value = Liquid::Template.parse(value).render({ "site" => { "data" => @site.data }.merge(@site.config) })
+          value = Liquid::Template.parse(value).render({ 'site' => { 'data' => @site.data }.merge(@site.config) })
           @data[key] = Jekyll::Renderer.new(@site, self).convert(value)
           @data[key] = @data[key].sub(/^<p>/, '').sub(/<\/p>$/, '').strip
         end
@@ -32,6 +32,52 @@ module Jekyll
 
   class Site
     alias_method :old_read, :read
+    alias_method :old_read_data_to, :read_data_to
+
+    def in_source_dir(*paths)
+      paths.reduce(source) do |base, path|
+        Jekyll.sanitized_path(base, path)
+      end
+    end
+
+    # allows us to filter data file content out on conditionals, eg. `{% if page.version == ... %}`
+    def read_data_to(dir, data)
+      return unless File.directory?(dir) && (!safe || !File.symlink?(dir))
+
+      entries = Dir.chdir(dir) do
+        Dir['*.{yaml,yml,json,csv}'] + Dir['*'].select { |fn| File.directory?(fn) }
+      end
+
+      entries.each do |entry|
+        path = in_source_dir(dir, entry)
+        next if File.symlink?(path) && safe
+
+        key = sanitize_filename(File.basename(entry, '.*'))
+        if File.directory?(path)
+          read_data_to(path, data[key] = {})
+        else
+          case File.extname(path).downcase
+          when '.csv'
+            data[key] = CSV.read(path, :headers => true).map(&:to_hash)
+          else
+            contents = File.read(path)
+            if (matches = contents.scan /(\{% (?:if|unless).+? %\}.*?\{% end(?:if|unless) %\})/m) && config['data_file_variables']
+              filename = File.basename path
+              data_match = config['data_file_variables'].find { |d| d['names'].include? filename }
+              unless data_match.nil?
+                version = data_match['version']
+                temp_config = { 'page' => { 'version' => version } }
+                temp_config = self.config.merge(temp_config)
+                matches.each do |match|
+                  contents = contents.sub(match.first, Liquid::Template.parse(match.first).render(temp_config))
+                end
+              end
+            end
+            data[key] = SafeYAML.load(contents)
+          end
+        end
+      end
+    end
 
     # allow us to use any variable within Jekyll data files; for example:
     # - '{{ site.data.conrefs.product_name[site.audience] }} Glossary'
